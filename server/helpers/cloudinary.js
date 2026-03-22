bb2d9e8ac8d7352f6a60f60bdbce6d4775c16da6
Rash Estebek <98695442+RashEstebek@users.noreply.github.com>
2026-03-22 15:28:00 +0500

    some changes

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 
 cloudinary.config({
-  cloud_name: "dzxseo8p2",
-  api_key: "245883613516771",
-  api_secret: "md5uJML9TWW6CBylaIMvHCagtxk",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new multer.memoryStorage();
--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -2,8 +2,8 @@ const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
   mode: "sandbox",
-  client_id: "AXEjMZjzDQDWFWvg-iHpXaTsFcVOpjDMVjlBx4KWeGS9d9WDn2NMgk1krqxzdF_r0q3G4bARYY3R66MK",
-  client_secret: "EEXGnNpUMLj-RsZS0NsI4H2A7QphG6LDLsiFRdHN3QtybGyrz1pMR13FJtmrnQq9xwyHTHT-HPZSMEn8",
+  client_id: process.env.PAYPAL_CLIENT_ID,
+  client_secret: process.env.PAYPAL_CLIENT_SECRET,
 });
 
 module.exports = paypal;
